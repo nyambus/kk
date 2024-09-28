4023b0a94cf4326e0d8085fba506a838fd432d98
nyambus <nxmbisdead@gmail.com>
2024-09-28 14:17:48 +0500

    Update install_wp.sh

--- scripts/install_wp.sh
+++ scripts/install_wp.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-sudo apt-get update -y && sudo apt-get upgrade -y
+apt-get update -y && apt-get upgrade -y
 
 # Установка Apache2
-sudo apt install apache2 -y
+apt install apache2 -y
 
 # Установка PHP
-sudo apt-get install php8.2 php8.2-cli php8.2-common php8.2-imap php8.2-redis php8.2-snmp php8.2-xml php8.2-mysqli php8.2-zip php8.2-mbstring php8.2-curl libapache2-mod-php -y
+apt-get install php8.2 php8.2-cli php8.2-common php8.2-imap php8.2-redis php8.2-snmp php8.2-xml php8.2-mysqli php8.2-zip php8.2-mbstring php8.2-curl libapache2-mod-php -y
 
 # Установка MariaDB
-sudo apt install mariadb-server -y
+apt install mariadb-server -y
 
 # Настройка базы данных
-sudo mariadb <<EOF
+mariadb <<EOF
 CREATE USER 'wordpress'@'localhost' IDENTIFIED BY 'kk'; 
 CREATE DATABASE 'wordpress'; 
 GRANT ALL PRIVILEGES ON wordpress.* TO 'wordpress'@'localhost'; 
@@ -27,27 +27,27 @@ unzip latest.zip
 rm latest.zip
 
 # Установка прав на файлы и директории
-sudo chown -R www-data:www-data wordpress/
+chown -R www-data:www-data wordpress/
 cd wordpress/
-sudo find . -type d -exec chmod 755 {} \;
-sudo find . -type f -exec chmod 644 {} \;
+find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
 
 # Настройка файла wp-config.php
 cd /var/www/html/wordpress
 mv wp-config-sample.php wp-config.php
 
 # Изменение конфигурации wp-config.php
-sudo sed -i "s/database_name_here/wordpress/" wp-config.php
-sudo sed -i "s/username_here/wordpress/" wp-config.php
-sudo sed -i "s/password_here/kk/" wp-config.php
+sed -i "s/database_name_here/wordpress/" wp-config.php
+sed -i "s/username_here/wordpress/" wp-config.php
+sed -i "s/password_here/kk/" wp-config.php
 
 # Создание конфигурационного файла для Apache
 cd /etc/apache2/sites-available/
-sudo touch wordpress.conf
+touch wordpress.conf
 
 # Заполнение wordpress.conf
 IP_ADDRESS=$(hostname -I | awk '{print $1}')
-sudo bash -c 'cat <<EOL > wordpress.conf
+bash -c 'cat <<EOL > wordpress.conf
 <VirtualHost *:80>
     ServerName $IP_ADDRESS
     DocumentRoot /var/www/html/wordpress
@@ -60,11 +60,11 @@ sudo bash -c 'cat <<EOL > wordpress.conf
 EOL'
 
 # Активация модуля перезаписи и конфигурации сайта
-sudo a2enmod rewrite
-sudo a2ensite wordpress.conf
+a2enmod rewrite
+a2ensite wordpress.conf
 
 # Проверка синтаксиса конфигурации Apache
-sudo apachectl -t
+apachectl -t
 
 # Перезагрузка Apache
-sudo systemctl reload apache2
+systemctl reload apache2
