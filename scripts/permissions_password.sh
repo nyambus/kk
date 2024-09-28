47a0b1e08a3293d31e964ef2f04bdae3ac0c6eaf
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-09-29 02:04:39 +0500

    Update permissions_password.sh

--- scripts/permissions_password.sh
+++ scripts/permissions_password.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
 
+#Смена пароля для рута
+passwd root << EOF
+S>V5>pk@t4*JQV4U
+S>V5>pk@t4*JQV4U
+EOF
+
+#Пользователь для брутфорса фтп
+adduser IvanPopov_IT --allow-bad-names << EOF
+12345
+12345
+EOF
+
+mkdir /home/IvanPopov_IT/FilesForMyFirstJob
+ln -s /var/www/html/wordpress/wp-includes/user.php /home/IvanovPopov_IT/FilesForMyFirstJob/
