63615164e3060bdd6ca5918149fce0523d479221
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-10-06 04:16:34 +0500

    Update network.sh

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
+echo '# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
 
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+
+#???
+auto ens36
+iface ens36 inet static
+        address 192.168.122.205/24
+        gateway 192.168.122.1' > /etc/network/interfaces
+
+echo 'nameserver 192.168.122.1
+nameserver 8.8.8.8' > /etc/resolv.conf
