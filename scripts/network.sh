4413ad0f2c024586310c3dd76c96acdd37734fac
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-10-06 16:58:54 +0500

    Update network.sh

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Определяем имя первого активного физического сетевого интерфейса
-INTERFACE=$(ip -o -f inet addr show | awk '!/lo/ && /inet / {print $2; exit}')
-
-if [ -z "$INTERFACE" ]; then
-    echo "Нет активного интерфейса с IP-адресом!"
-    exit 1
-fi
-
 # Генерируем файл /etc/network/interfaces
 echo '# This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
@@ -16,8 +8,8 @@ source /etc/network/interfaces.d/*
 
 auto lo
 iface lo inet loopback
-auto '$INTERFACE'
-iface '$INTERFACE' inet static
+auto enp1s0
+iface enp1s0 inet static
         address 192.168.122.205/24
         gateway 192.168.122.1' > /etc/network/interfaces
 
