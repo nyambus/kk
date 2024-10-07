3d71c5b996dc2ee4d58279ca4073abe2b7da0df5
nyambus <nxmbisdead@gmail.com>
2024-10-07 20:16:35 +0500

    Update network.sh

--- scripts/network.sh
+++ scripts/network.sh
@@ -11,10 +11,10 @@ iface lo inet loopback
 auto enp1s0
 iface enp1s0 inet static
         address 192.168.122.205/24
-        gateway 192.168.122.1' > /etc/network/interfaces
+        gateway 192.168.122.2' > /etc/network/interfaces
 
 # Создаем файл /etc/resolv.conf
-echo 'nameserver 192.168.122.1
+echo 'nameserver 192.168.122.2
 nameserver 8.8.8.8' > /etc/resolv.conf
 
 systemctl restart networking
