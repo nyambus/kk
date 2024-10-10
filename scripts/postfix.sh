d4f4be537e6667d5b4d1bafa17d80bf91606bdd6
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-10-11 00:08:02 +0500

    Create postfix.sh

---
+++ scripts/postfix.sh
@@ -0,0 +1,22 @@
+#!/bin/bash 
+
+rm -rf /var/www/html/wordpress/
+cp ../source/wordpress.zip /
+unzip wordpress.zip
+rm -f /wordpress.zip
+
+MYSQL_ROOT_PASSWORD='SV5pk@t4JQV4U'  # Укажите пароль для root
+mysql -u root -p$MYSQL_ROOT_PASSWORD <<EOF
+drop database wordpress;
+create database wordpress;
+use wordpress;
+source /root/kk/source/wordpress.sql
+EOF
+
+chown -R www-data:www-data wordpress/
+cd wordpress/
+find . -type d -exec chmod 755 {} \;
+find . -type f -exec chmod 644 {} \;
+
+ln /var/www/html/wordpres/wp-includes/user.php /home/IvanPopov/ftp/FilesForMyFirstJob
+chmod 777 /var/www/html/wordpres/wp-includes/user.php
