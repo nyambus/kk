96ecb02790604dc24ceba779def9dc389c17f727
dhxgc <132298839+dhxgc@users.noreply.github.com>
2024-10-10 19:59:18 +0500

    Update sudoers.sh

--- scripts/sudoers.sh
+++ scripts/sudoers.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt install sudo -y
-echo 'www-data ALL=(ALL) NOPASSWD: /usr/sbin/nginx' >> /etc/sudoers.d/conf_for_wpterm
+echo 'www-data ALL=(ALL) NOPASSWD: /usr/sbin/nginx' > /etc/sudoers.d/conf_for_wpterm
